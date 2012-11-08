7c0b7fe4661658edd1a419bb6dc070048ffd5587
Dustin Dettmer <dustinpaystaxes@gmail.com>
2012-11-08 14:46:43 -0800

    Had a few missing files.

---
+++ BigShotsGetShots/ViewController.m
@@ -0,0 +1,175 @@
+//
+//  DetailViewController.m
+//  SVGPad
+//
+//  Copyright Matt Rajca 2010-2011. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, retain) UIPopoverController *popoverController;
+
+- (void)loadResource:(NSString *)name;
+- (void)shakeHead;
+
+@end
+
+
+@implementation ViewController
+
+- (NSString *)docPath
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    return [documentsDirectory stringByAppendingString:@"/"];
+}
+
+@synthesize scrollView;
+
+@synthesize toolbar, popoverController, contentView, detailItem;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)setDetailItem:(id)newDetailItem {
+	if (detailItem != newDetailItem) {
+        
+		[self loadResource:newDetailItem];
+	}
+	
+	if (self.popoverController) {
+		[self.popoverController dismissPopoverAnimated:YES];
+	}
+}
+
+
+
+- (void)loadResource:(NSString *)name
+{
+    [self.contentView removeFromSuperview];
+    
+	SVGDocument *document = [SVGDocument documentNamed:[name stringByAppendingPathExtension:@"svg"]];
+	NSLog(@"[%@] Freshly loaded document (name = %@) has width,height = (%.2f, %.2f)", [self class], name, document.width, document.height );
+	self.contentView = [[SVGView alloc] initWithDocument:document];
+	
+	_name = [name copy];
+    
+    [self.scrollView addSubview:self.contentView];
+    [self.scrollView setContentSize:CGSizeMake(document.width, document.height)];
+    [self.scrollView zoomToRect:CGRectMake(0, 0, document.width * .75, document.height * .55) animated:NO];
+}
+
+- (IBAction)animate:(id)sender {
+	if ([_name isEqualToString:@"Monkey"]) {
+		[self shakeHead];
+	}
+}
+
+
+- (void)shakeHead {
+	CALayer *layer = [self.contentView.document layerWithIdentifier:@"head"];
+	
+	CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+	animation.duration = 0.25f;
+	animation.autoreverses = YES;
+	animation.repeatCount = 100000;
+	animation.fromValue = [NSNumber numberWithFloat:0.1f];
+	animation.toValue = [NSNumber numberWithFloat:-0.1f];
+	
+	[layer addAnimation:animation forKey:@"shakingHead"];
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+	 willHideViewController:(UIViewController *)aViewController
+		  withBarButtonItem:(UIBarButtonItem *)barButtonItem
+	   forPopoverController:(UIPopoverController *)pc {
+	
+	barButtonItem.title = @"Samples";
+	
+	NSMutableArray *items = [[toolbar items] mutableCopy];
+	[items insertObject:barButtonItem atIndex:0];
+	
+	[toolbar setItems:items animated:YES];
+	
+	self.popoverController = pc;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+	 willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
+	
+	NSMutableArray *items = [[toolbar items] mutableCopy];
+	[items removeObjectAtIndex:0];
+	
+	[toolbar setItems:items animated:YES];
+	
+	self.popoverController = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.contentView;
+}
+
+#pragma mark Export
+
+
+- (IBAction)exportLayers:(id)sender {
+    if (_layerExporter) {
+        return;
+    }
+    _layerExporter = [[CALayerExporter alloc] initWithView:contentView];
+    _layerExporter.delegate = self;
+    
+    UITextView* textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 400, 400)];
+    UIViewController* textViewController = [[UIViewController alloc] init];
+    [textViewController setView:textView];
+    UIPopoverController* exportPopover = [[UIPopoverController alloc] initWithContentViewController:textViewController];
+    [exportPopover setDelegate:self];
+    [exportPopover presentPopoverFromBarButtonItem:sender
+                    permittedArrowDirections:UIPopoverArrowDirectionAny
+                                    animated:YES];
+    
+    _exportText = textView;
+    _exportText.text = @"exporting...";
+    
+    _exportLog = [[NSMutableString alloc] init];
+    [_layerExporter startExport];
+}
+
+- (void) layerExporter:(CALayerExporter*)exporter didParseLayer:(CALayer*)layer withStatement:(NSString*)statement
+{
+    //NSLog(@"%@", statement);
+    [_exportLog appendString:statement];
+    [_exportLog appendString:@"\n"];
+}
+
+- (void)layerExporterDidFinish:(CALayerExporter *)exporter
+{
+    _exportText.text = _exportLog;
+}
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController *)pc
+{
+    _exportText = nil;
+    _layerExporter = nil;
+}
+
+
+- (void)viewDidUnload {
+    [self setScrollView:nil];
+    [super viewDidUnload];
+}
+
+
+
+@end
